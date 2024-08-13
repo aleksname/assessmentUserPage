37b04a56f702dd0a1ab6b92b3a455ce9e843d397
aleksname <aleksname.v2@gmail.com>
2024-08-13 11:28:40 +0300

    feat: adding recomended page lobbi with select next page

--- pages/recommended.jsx
+++ pages/recommended.jsx
@@ -1,7 +1,42 @@
 import React from 'react'
+import MainContainer from './MainContainer'
+import styles from './styles/style.module.scss'
+import Image from 'next/image'
+import Link from 'next/link';
+import CircleIcon from '../public/RecomendedPage/CircleIcon.svg'
+import RadiusIcon from '../public/RecomendedPage/RadiusIcon.svg'
+import Diametericon from '../public/RecomendedPage/Diametericon.svg'
+import CanvasIcon from '../public/RecomendedPage/CanvasIcon.svg'
 
 export default function recommended() {
+
   return (
-    <div>recommended</div>
+    <MainContainer>
+      <div className={styles.wrapperRecomendedPage}>
+        <div className={styles.recomendedPageBlock}>
+          <div className={styles.recommendedLinkBlock}>
+            <Link href="#" className={styles.recommendedLink}>
+              <Image src={CircleIcon} alt='CircleIcon'/>
+              <div className={styles.circleIconTitle}>Circle</div>  
+            </Link>
+
+            <Link href="#" className={styles.recommendedLink}>
+              <Image src={RadiusIcon} alt='RadiusIcon'/>
+              <div className={styles.circleIconTitle}>Radius</div>  
+            </Link>
+
+            <Link href="#" className={styles.recommendedLink}>
+              <Image src={Diametericon} alt='Diametericon'/>
+              <div className={styles.circleIconTitle}>Diameter</div>  
+            </Link>
+
+            <Link href="#" className={styles.recommendedLink}>
+              <Image src={CanvasIcon} alt='Diametericon'/>
+              <div className={styles.circleIconTitle}>Canvas</div>  
+            </Link>
+          </div>
+        </div>
+      </div>
+    </MainContainer>
   )
 }
