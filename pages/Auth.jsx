e751063e0b315901c40a3b88e2357583ac5538bb
aleksname <aleksname.v2@gmail.com>
2024-09-09 10:42:05 +0300

    fix: realy Xz....

--- pages/Auth.jsx
+++ pages/Auth.jsx
@@ -1,23 +1,24 @@
 import { useEffect, useState } from "react";
 import { getAuth, signInWithPopup } from "firebase/auth";
-import app, { googleAuthProvider } from "../firebase";
+import { app, googleAuthProvider } from "../firebase";
+
 
 export default function Auth() {
     const auth = getAuth(app);
     const [user, setUser] = useState(auth.currentUser);
 
     useEffect(() => {
-        const unSub = auth.onAuthStateChanged((maybeUser) => {
+       const unSub = auth.onAuthStateChanged((maybeUser) => {
             if (maybeUser != null) {
-                setUser(maybeUser);
-            } else {
-                signInWithPopup(auth, googleAuthProvider)
-                    .then((credentials) => setUser(credentials.user))
-                    .catch((e) => console.error(e));
+                return setUser(maybeUser);
             }
+
+            signInWithPopup(auth, googleAuthProvider)
+                .then((credentials) => setUser(credentials.user))
+                .catch((e) => console.error(e))
         });
         return unSub;
     }, [auth]);
 
-    return user != null ? <>{user.displayName}</> : <>loading..</>;
+    return user != null ? <>{ userAgent.displayName }</> : <>loading..</>;
 }
