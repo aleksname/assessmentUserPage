b923f0e593ed16b5b918bd7cea171d161c1e8279
aleksname <aleksname.v2@gmail.com>
2024-09-11 14:29:35 +0300

    feat: remove firebase auth..

--- pages/LearnHeadBlock.jsx
+++ pages/LearnHeadBlock.jsx
@@ -1,27 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React  from 'react';
 import styles from './styles/style.module.scss';
-import headCoin from '../public/MainPage/Coin.png';
 import Image from 'next/image';
 import BackHandIcon from '@mui/icons-material/BackHand';
 import Usericon from '../public/profile/profileIcon2.svg';
 import { Link } from '@mui/material';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { app } from '../firebase';
 
 export default function LearnHeadBlock() {
-  const [user, setUser] = useState(null); // Зберігаємо користувача в стані
-  const auth = getAuth(app);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (maybeUser) => {
-      if (maybeUser) {
-        setUser(maybeUser); // Якщо користувач залогінений, зберігаємо його дані
-      }
-    });
-
-    return () => unsubscribe(); // Чистка підписки
-  }, [auth]);
-
   return (
     <>
       <div className={styles.learnHeadBlock}>
@@ -33,12 +17,10 @@ export default function LearnHeadBlock() {
         <div className={styles.headUserNameBlock}>
           <div className={styles.headUserInfo}>
             <div className={styles.headUserName}>
-              <BackHandIcon style={{ color: 'yellow' }} className={styles.headUserNameImg} />
+              {/* <BackHandIcon style={{ color: 'yellow' }} className={styles.headUserNameImg} /> */}
               <pre className="">HI, </pre>        
-              {/* Відображаємо ім'я користувача або email, якщо ім'я не встановлено */}
-              <div className={styles.userTitle}>{user ? user.displayName || user.email : 'Guest'}</div>
+              <div className={styles.userTitle}>Teacher Oleksii</div>
             </div>
-            {/* Тут залишаємо статичний текст */}
             <div className={styles.userSubTitle}>MINE_1y_66_04_10_23</div>
           </div>
           <Link href="/profile" className="">
